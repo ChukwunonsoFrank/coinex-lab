58fe1b17ed0d4955db7e26cebcb65062044ec585
Chukwunonso Frank <franknonso114@gmail.com>
2025-08-22 00:14:50 +0100

    wip

--- resources/views/livewire/dashboard/support.blade.php
+++ resources/views/livewire/dashboard/support.blade.php
@@ -2,7 +2,7 @@
     <div class="lg:flex h-full">
         <livewire:dashboard.partials.desktop-navbar />
         <div class="h-full w-full flex-1">
-            <iframe frameborder="0" width="100%" height="100%" src="https://jivo.chat/HjwhNMiL2E"></iframe>
+            <iframe frameborder="0" width="100%" height="100%" src="https://jivo.chat/haRE265EfB"></iframe>
         </div>
     </div>
 
